0236a0f0c84089fc7f16f905f756f96a6163e1fc
Andy Likuski <andy@likuski.org>
2018-01-16 15:30:16 -0800

    Added all helpers

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,114 @@
-import Example from './components/Example';
-import SecondExample from './components/SecondExample';
-
-export default Example;
+import {apolloContainerTests} from 'src/lib/apolloContainerTestHelpers';
+import {loadSpreadsheet} from 'src/lib/authHelpers';
+import {
+  applyIfFunction,
+  applyToIfFunction, composeViews, composeViewsFromStruct,
+  eMap, itemizeProps, joinComponents, keyWith, liftAndExtract, liftAndExtractItems, loadingCompleteStatus,
+  makeApolloTestPropsFunction,
+  makeTestPropsFunction,
+  mergeActionsForViews,
+  mergePropsForViews,
+  mergeStylesIntoViews,
+  nameLookup,
+  propLensEqual, propsAndStyle, propsFor,
+  propsForItem, propsForSansClass,
+  renderChoicepoint, renderErrorDefault, renderLoadingDefault
+} from 'src/lib/componentHelpers';
+import mergeDeepWith from 'ramda/es/mergeDeepWith';
+import {concatFeatures, featureByType, geojsonByType} from 'src/lib/geojsonHelpers';
+import {calculateDistance} from 'src/lib/geospatialHelpers';
+import {copy, fromImmutable, toImmutable, toImmutableKeyedByProp, toJS} from 'src/lib/immutableHelpers';
+import {
+  nodeToFeature, projectBoundingBox, sankeyGenerator, sankeyGeospatialTranslate, translateNodeFeature,
+  unprojectNode
+} from 'src/lib/sankeyHelpers';
+import {
+  applyStyleFunctionOrDefault,
+  createScaledPropertyGetter, getClass, getClassAndStyle, getStyleObj, styleArithmetic,
+  styleMultiplier
+} from 'src/lib/styleHelpers';
+import {resolveFeatureFromExtent, resolveSvgPoints, resolveSvgReact} from 'src/lib/svgHelpers';
+import {
+  asyncPropsFromSampleStateAndContainer, eitherToPromise,
+  expectTask, expectTaskRejected, makeMockStore, makeSampleInitialState, makeSampleStore, mockApolloClient,
+  mockApolloClientWithSamples,
+  propsFromSampleStateAndContainer, shallowWrap,
+  testState, waitForChildComponentRender, wrapWithMockGraphqlAndStore, wrapWithMockStore
+} from 'src/lib/testHelpers';
+import {toTimeString} from 'src/lib/timeHelpers';
 
 export {
-  SecondExample,
+  apolloContainerTests,
+  loadSpreadsheet,
+  loadingCompleteStatus,
+  propLensEqual,
+  eMap,
+  renderChoicepoint,
+  mergeActionsForViews,
+  mergePropsForViews,
+  mergeDeepWith,
+  keyWith,
+  applyToIfFunction,
+  applyIfFunction,
+  makeTestPropsFunction,
+  makeApolloTestPropsFunction,
+  liftAndExtract,
+  liftAndExtractItems,
+  mergeStylesIntoViews,
+  propsFor,
+  propsForSansClass,
+  propsAndStyle,
+  itemizeProps,
+  propsForItem,
+  nameLookup,
+  composeViews,
+  composeViewsFromStruct,
+  joinComponents,
+  renderLoadingDefault,
+  renderErrorDefault,
+  featureByType,
+  geojsonByType,
+  concatFeatures,
+  calculateDistance,
+  toImmutable,
+  toJS,
+  fromImmutable,
+  toImmutable,
+  toImmutableKeyedByProp,
+  copy,
+  sankeyGenerator,
+  unprojectNode,
+  sankeyGeospatialTranslate,
+  projectBoundingBox,
+  nodeToFeature,
+  translateNodeFeature,
+  getClass,
+  getClassAndStyle,
+  getStyleObj,
+  styleArithmetic,
+  styleMultiplier,
+  createScaledPropertyGetter,
+  applyStyleFunctionOrDefault,
+  resolveSvgPoints,
+  resolveSvgReact,
+  resolveFeatureFromExtent,
+  expectTask,
+  expectTaskRejected,
+  testState,
+  makeSampleStore,
+  makeSampleInitialState,
+  propsFromSampleStateAndContainer,
+  asyncPropsFromSampleStateAndContainer,
+  makeMockStore,
+  mockApolloClient,
+  mockApolloClientWithSamples,
+  wrapWithMockGraphqlAndStore,
+  wrapWithMockStore,
+  shallowWrap,
+  eitherToPromise,
+  waitForChildComponentRender,
+  toTimeString
 };
+
+
+
