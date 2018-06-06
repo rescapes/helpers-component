72b222fec71134d0b94060d4e9a02215ddaf1c68
Andy Likuski <andy@likuski.org>
2018-06-05 18:09:03 -0700

    v1.5.9

--- installPeers.js
+++
@@ -1,2 +0,0 @@
-const peers = Object.entries(require('./package.json').peerDependencies || {}).map(d => d.join('@')).join(' ');
-if (peers.length) process.stdout.write('yarn add -P --no-lockfile ' + String(peers));
