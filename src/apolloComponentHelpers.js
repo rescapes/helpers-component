8bdae2da7604a0a23d8f690fac413df7c4dc9eef
Andy Likuski <andy@likuski.org>
2018-11-27 16:11:03 +0100

    v1.5.25

---
+++ src/apolloComponentHelpers.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Andy Likuski on 2018.11.27
+ * Copyright (c) 2018 Andy Likuski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Composes all queries/mutations in queryDefinitions.
+ * @param {Object} queryDefinitions Object keyed by name and valued by an object containing a query and args.
+ * @param {String} queryDefinitions.query The query/mutation string. Not gql wrapped yet
+ * @param {Object} queryDefinitions.args Arguments for the query as per the graphql() function
+ * @param {Object|Function} queryDefinitions.args.options Query options like errorPolicy: 'all'. Can also be a function
+ * to set create query variables. Example:
+ * options: ({data: {region}}) => ({
+        variables: {
+          regionId: region.id
+        },
+        // Pass through error so we can handle it in the component
+        errorPolicy: 'all'
+      }),
+ * @param {Object|Function} queryDefinitions.args.prop Prop function that merge the Apollo data object with ownProps. Example:
+ props: ({data, ownProps}) => {
+        return mergeDeep(
+          ownProps,
+          {data}
+        )
+      }
+ */
+export const composeGraphqlQueryDefinitions = queryDefinitions => {
+  return R.compose(
+    // Each graphql call calls the next
+    ...R.map(queryDefintion => graphql(gql`${R.prop('query', queryDefintion)}`, R.prop('args', queryDefintion))),
+    R.values,
+  )(queryDefinitions)
+};
---
+++ src/apolloComponentHelpers.spec.js
@@ -0,0 +1,151 @@
+
+/**
+ * Query
+ * Prerequisites:
+ *   A Region
+ * Resolves:
+ *  The geojson of the Region
+ * Without prerequisites:
+ *  Skip render
+ */
+const geojsonQuery = `
+    query geojson($regionId: String!) {
+          region(id: $regionId) {
+              id
+              geojson {
+                  osm {
+                      features {
+                          id
+                          type
+                          geometry {
+                              type
+                              coordinates
+                          }
+                          properties
+                      }
+                  }
+                  sankey {
+                    graph {
+                        nodes {
+                            siteName
+                            location
+                            coordinates
+                            junctionStage
+                            annualTonnage
+                            index
+                            material
+                            isGeneralized
+                            type
+                            geometry {
+                              type
+                              coordinates
+                            }
+                            name
+                            value
+                        }
+                        links {
+                          source 
+                          target 
+                          value
+                        }
+                    }
+                    stages {
+                      key
+                      name
+                      target
+                    }
+                    stageKey
+                    valueKey
+                  }
+              }
+        }
+    }
+`;
+
+/**
+ * Mutates the selected filterNodeCategories by passing the category and value that have changed
+ * @type {string}
+ */
+const filterSankeyNodesMutation = `
+    mutation filterSankeyNodesMutation($filterNodeCategory: String!, $filterNodeValue: Boolean!) {
+        filterSankeyNodesMutation(filterNodeCategory: $filterNodeCategory, filterNodeValue: $filterNodeValue) {
+          material
+        }
+    }
+`;
+
+const onViewportChangeMutation = `
+  mutation onViewportChange() {
+  }
+`;
+
+/**
+ * All queries used by the container
+ */
+export const queries = {
+  /**
+   * Expects a region with an id and resolves geojson of the region
+   */
+  geojson: {
+    query: geojsonQuery,
+    args: {
+      options: ({data: {region}}) => ({
+        variables: {
+          regionId: region.id
+        },
+        errorPolicy: 'all'
+      }),
+      props: ({data, ownProps}) => {
+        let filteredData = data;
+        if (data.store) {
+          // Who is the user of the region
+          const userRegion = R.find(R.eqProps('id', ownProps.data.region), ownProps.data.user.regions);
+          // Get all selected categories that are marked true
+          const selectedSankeyNodeCategories =
+            R.filter(
+              R.identity,
+              R.defaultTo({}, R.view(R.lensPath(['geojson', 'sankey', 'selected']), userRegion)));
+
+          // If there are any selected categories, set them deep down in the sankey data
+          filteredData = R.length(selectedSankeyNodeCategories) ?
+            R.over(
+              R.lensPath(['region', 'geojson', 'sankey', 'graph', 'nodes']),
+              nodes => R.map(node => R.merge(
+                node,
+                {
+                  isVisible: R.or(
+                    // Not there
+                    R.compose(R.isNil, R.prop(node.material))(selectedSankeyNodeCategories),
+                    // There and true
+                    R.contains(node.material, R.keys(selectedSankeyNodeCategories))
+                  )
+                }
+              ), nodes || []),
+              data
+            ) : data;
+        }
+        // Merge the work we did with the rest of the props
+        return mergeDeep(
+          ownProps,
+          {data: filteredData}
+        );
+      }
+    }
+  },
+  filterSankeyNodesMutation: {
+    query: filterSankeyNodesMutation,
+    args: {
+      options: {
+        errorPolicy: 'all'
+      },
+      props: ({mutate}) => ({
+        onSankeyFilterChange:
+          (filterNodeCategory, filterNodeValue) => mutate({variables: {filterNodeCategory, filterNodeValue}})
+      })
+    }
+  }
+};
+
+describe('apolloComponentHelpers', ())
+// Create the GraphQL Container.
+const ContainerWithData = composeGraphqlQueryDefinitions(queries)(Sankey);
